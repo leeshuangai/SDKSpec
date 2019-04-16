2dc84fc8e43563638700b962189a47eaeb92f099
lishuang <lishuang@51xl.com>
2019-04-16 09:36:44 +0800

    提交描述

--- Example/HYComponent/HYAppDelegate.m
+++ Example/HYComponent/HYAppDelegate.m
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    // 测试地阿米
     return YES;
 }
 
---
+++ Example/Pods/Target Support Files/HYComponent/HYComponent-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HYComponent : NSObject
+@end
+@implementation PodsDummy_HYComponent
+@end
